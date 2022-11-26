4dfd4bb7274d9d1933bcf782354d1d5678934c13
mahadidev7 <mahadidev7@gmail.com>
2022-11-26 19:26:04 +0600

    footer part some update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,42 +8,42 @@ function Footer() {
             <div className='grid grid-cols-2 md:grid-cols-4 gap-9 pb-10'>
                 <div className="text-sm flex flex-col">
                     <p className="text-xl text-white my-2">Get to Know Us</p>
-                    <a className="mb-1 hover:underline" href="https://mahadidev7-portfolio.web.app/">Careers</a>
-                    <a className="mb-1 hover:underline" href="https://mahadidev7-portfolio.web.app/">Blog</a>
-                    <a className="mb-1 hover:underline" href="https://mahadidev7-portfolio.web.app/">About Amazon</a>
-                    <a className="mb-1 hover:underline" href="https://mahadidev7-portfolio.web.app/">Investor Relations</a>
-                    <a className="mb-1 hover:underline" href="https://mahadidev7-portfolio.web.app/">Amazon Devices</a>
-                    <a className="mb-1 hover:underline" href="https://mahadidev7-portfolio.web.app/">Amazon Science</a>
+                    <a className="mb-1 hover:underline" target="__blank" href="https://mahadidev7-portfolio.web.app/">Careers</a>
+                    <a className="mb-1 hover:underline" target="__blank" href="https://mahadidev7-portfolio.web.app/">Blog</a>
+                    <a className="mb-1 hover:underline" target="__blank" href="https://mahadidev7-portfolio.web.app/">About Amazon</a>
+                    <a className="mb-1 hover:underline" target="__blank" href="https://mahadidev7-portfolio.web.app/">Investor Relations</a>
+                    <a className="mb-1 hover:underline" target="__blank" href="https://mahadidev7-portfolio.web.app/">Amazon Devices</a>
+                    <a className="mb-1 hover:underline" target="__blank" href="https://mahadidev7-portfolio.web.app/">Amazon Science</a>
                 </div>
                 <div className="text-sm flex flex-col">
                     <p className="text-xl text-white my-2">Make Money with Us</p>
-                    <a className="mb-1 hover:underline" href="https://mahadidev7-portfolio.web.app/">Sell products on Amazon</a>
-                    <a className="mb-1 hover:underline" href="https://mahadidev7-portfolio.web.app/">Amazon Science</a>
-                    <a className="mb-1 hover:underline" href="https://mahadidev7-portfolio.web.app/">Sell on Amazon Business</a>
-                    <a className="mb-1 hover:underline" href="https://mahadidev7-portfolio.web.app/">Investor Relations</a>
-                    <a className="mb-1 hover:underline" href="https://mahadidev7-portfolio.web.app/">Amazon Devices</a>
-                    <a className="mb-1 hover:underline" href="https://mahadidev7-portfolio.web.app/">About Amazon</a>
+                    <a className="mb-1 hover:underline" target="__blank" href="https://mahadidev7-portfolio.web.app/">Sell products on Amazon</a>
+                    <a className="mb-1 hover:underline" target="__blank" href="https://mahadidev7-portfolio.web.app/">Amazon Science</a>
+                    <a className="mb-1 hover:underline" target="__blank" href="https://mahadidev7-portfolio.web.app/">Sell on Amazon Business</a>
+                    <a className="mb-1 hover:underline" target="__blank" href="https://mahadidev7-portfolio.web.app/">Investor Relations</a>
+                    <a className="mb-1 hover:underline" target="__blank" href="https://mahadidev7-portfolio.web.app/">Amazon Devices</a>
+                    <a className="mb-1 hover:underline" target="__blank" href="https://mahadidev7-portfolio.web.app/">About Amazon</a>
                 </div>
                 <div className="text-sm flex flex-col">
                     <p className="text-xl text-white my-2">Get to Know Us</p>
-                    <a className="mb-1 hover:underline" href="https://mahadidev7-portfolio.web.app/">Careers</a>
-                    <a className="mb-1 hover:underline" href="https://mahadidev7-portfolio.web.app/">Blog</a>
-                    <a className="mb-1 hover:underline" href="https://mahadidev7-portfolio.web.app/">About Amazon</a>
-                    <a className="mb-1 hover:underline" href="https://mahadidev7-portfolio.web.app/">Investor Relations</a>
-                    <a className="mb-1 hover:underline" href="https://mahadidev7-portfolio.web.app/">Amazon Devices</a>
-                    <a className="mb-1 hover:underline" href="https://mahadidev7-portfolio.web.app/">Amazon Science</a>
+                    <a className="mb-1 hover:underline" target="__blank" href="https://mahadidev7-portfolio.web.app/">Careers</a>
+                    <a className="mb-1 hover:underline" target="__blank" href="https://mahadidev7-portfolio.web.app/">Blog</a>
+                    <a className="mb-1 hover:underline" target="__blank" href="https://mahadidev7-portfolio.web.app/">About Amazon</a>
+                    <a className="mb-1 hover:underline" target="__blank" href="https://mahadidev7-portfolio.web.app/">Investor Relations</a>
+                    <a className="mb-1 hover:underline" target="__blank" href="https://mahadidev7-portfolio.web.app/">Amazon Devices</a>
+                    <a className="mb-1 hover:underline" target="__blank" href="https://mahadidev7-portfolio.web.app/">Amazon Science</a>
                 </div>
                 <div className="text-sm flex flex-col">
                     <p className="text-xl text-white my-2">Make Money with Us</p>
-                    <a className="mb-1 hover:underline" href="https://mahadidev7-portfolio.web.app/">Sell products on Amazon</a>
-                    <a className="mb-1 hover:underline" href="https://mahadidev7-portfolio.web.app/">Amazon Science</a>
-                    <a className="mb-1 hover:underline" href="https://mahadidev7-portfolio.web.app/">Sell on Amazon Business</a>
-                    <a className="mb-1 hover:underline" href="https://mahadidev7-portfolio.web.app/">Investor Relations</a>
-                    <a className="mb-1 hover:underline" href="https://mahadidev7-portfolio.web.app/">Amazon Devices</a>
-                    <a className="mb-1 hover:underline" href="https://mahadidev7-portfolio.web.app/">About Amazon</a>
+                    <a className="mb-1 hover:underline" target="__blank" href="https://mahadidev7-portfolio.web.app/">Sell products on Amazon</a>
+                    <a className="mb-1 hover:underline" target="__blank" href="https://mahadidev7-portfolio.web.app/">Amazon Science</a>
+                    <a className="mb-1 hover:underline" target="__blank" href="https://mahadidev7-portfolio.web.app/">Sell on Amazon Business</a>
+                    <a className="mb-1 hover:underline" target="__blank" href="https://mahadidev7-portfolio.web.app/">Investor Relations</a>
+                    <a className="mb-1 hover:underline" target="__blank" href="https://mahadidev7-portfolio.web.app/">Amazon Devices</a>
+                    <a className="mb-1 hover:underline" target="__blank" href="https://mahadidev7-portfolio.web.app/">About Amazon</a>
                 </div>
             </div>
-            <div className='mt-10 flex justify-center pt-5 pb-10'>
+            <div className='mt-10 flex justify-between pt-5 pb-10'>
                 <Image
                     src= "https://links.papareact.com/f90"
                     width={150}
@@ -51,6 +51,7 @@ function Footer() {
                     objectFit="contain"
                     className='cursor-pointer'
                 />
+                <p>This is a Portfolio Demo Website. create by <a className='underline bg-[#090d11] p-2 rounded' target="__blank" href="https://mahadidev7-portfolio.web.app/">Mahadidev7</a></p>
             </div>
         </div>
         
